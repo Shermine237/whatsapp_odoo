72593093391bd499e339c3b76c651d1190ce55f6
Charlie Rostant YOSSA <Charlieyossa@gmail.com>
2024-04-15 16:56:21 +0200

    Firts commit

--- __manifest__.py
+++ __manifest__.py
@@ -1,24 +1,4 @@
-# -*- coding: utf-8 -*-
-#############################################################################
-#
-#    Cybrosys Technologies Pvt. Ltd.
-#
-#    Copyright (C) 2021-TODAY Cybrosys Technologies(<https://www.cybrosys.com>)
-#    Author: Cybrosys Techno Solutions(<https://www.cybrosys.com>)
-#
-#    You can modify it under the terms of the GNU LESSER
-#    GENERAL PUBLIC LICENSE (LGPL v3), Version 3.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU LESSER GENERAL PUBLIC LICENSE (LGPL v3) for more details.
-#
-#    You should have received a copy of the GNU LESSER GENERAL PUBLIC LICENSE
-#    (LGPL v3) along with this program.
-#    If not, see <http://www.gnu.org/licenses/>.
-#
-#############################################################################
+
 {
     'name': 'Odoo Whatsapp Connector',
     'version': '16.0.0.1.0',
@@ -27,10 +7,7 @@
     'description': """Added options for sending Whatsapp messages and Mails in systray bar,sale order, invoices, 
     website portal view and share the access url of documents using share option available in each records through 
     Whatsapp web..""",
-    'author': 'Cybrosys Techno Solutions',
-    'website': "https://www.cybrosys.com",
-    'company': 'Cybrosys Techno Solutions',
-    'maintainer': 'Cybrosys Techno Solutions',
+    'author': '...',
     'depends': ['sale', 'account', 'website','sale_management'],
     'data': [
         'security/ir.model.access.csv',
